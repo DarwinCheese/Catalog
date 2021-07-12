080146bdc14d9d24df284008345bcccdba9a7ffe
Darwin Gutierrez <dgu@netcompany.com>
2021-07-12 13:17:31 +0200

    Chapter 3 done - DTOs and DI

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using Catalog.Entities;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Catalog.DTOs;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select( item => item.AsDto());
+            return items;
+        }
+
+        //GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id) 
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item.AsDto());
+        }
+    }
+}
